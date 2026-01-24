edb294f1a4bc4e7d13c61c4ea5f5d091582f3e9d
zania <itzmstep@gmail.com>
2026-01-25 00:11:45 +0200

    first

--- dz5.py
+++ dz5.py
@@ -1,27 +1,17 @@
 import colorama
 from colorama import Fore, Back, Style
 
-# Ініціалізація colorama (обовʼязково для Windows)
 colorama.init()
 
-# Інтроспекція бібліотеки
-print("Атрибути модуля colorama:")
-print(dir(colorama))
+help(colorama)
+help(Fore)
+help(Back)
+help(Style)
 
-print("\nАтрибути Fore:")
-print(dir(Fore))
-
-print("\nАтрибути Back:")
-print(dir(Back))
-
-print("\nАтрибути Style:")
-print(dir(Style))
-
-# Демонстрація роботи основних атрибутів
 print(Fore.RED + "Червоний текст")
 print(Fore.GREEN + "Зелений текст")
-print(Back.YELLOW + "Жовтий фон")
+print(Back.BLUE + "Синій фон")
 print(Style.BRIGHT + "Яскравий текст")
 
-# Скидання стилів
 print(Style.RESET_ALL + "Звичайний текст")
+    
